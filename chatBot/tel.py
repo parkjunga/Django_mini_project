fcba77beb6464acc8afb0b9809cae5a97450ae79
박정아 <>
2020-08-13 16:00:17 +0900

    제거

--- chatBot/tel.py
+++
@@ -1,9 +0,0 @@
-import telegram
-import key
-
-token = key.TELEGRAM_KEY
-bot = telegram.Bot(token=token)
-for i in bot.getUpdates():
-     print(i.message)
-
-bot.sendMessage(chat_id=1242107778 ,text='오늘날씨좋아')
